e3ccef480de5ef460ff68e7894fa66334375ccfe
davek411 <rajat12812324@gmail.com>
2024-06-28 13:12:15 +0530

    Standardizing the architecture

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -1,6 +1,9 @@
-package com.example.book.controller;
+package com.example.user.controller;
 
+import com.example.user.models.User;
+import com.example.user.services.UserService;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
@@ -8,9 +11,11 @@ import io.vertx.ext.web.RoutingContext;
 import com.example.book.models.Book;
 import com.example.book.services.BookService;
 
+import java.util.List;
+
 public class UserController extends AbstractVerticle {
 
-//  private BookService bookService = new BookService();
+  private UserService userService = new UserService();
 
   @Override
   public void start() {
@@ -18,13 +23,13 @@ public class UserController extends AbstractVerticle {
     Router router = Router.router(vertx);
 
     router.get("/books").handler(this::getAllUsers);
-    router.post("/books").handler(this::addUsers);
+    router.post("/books").handler(this::addUser);
 
     server.requestHandler(router).listen(config().getInteger("port", 8082));
   }
 // Example content for src/main/java/com/example/user/controller/UserController.java
-private void getAllUsers(RoutingContext context) {
-  bookService.getAllUsers(ar -> {
+private void getAllUsers(Future<List<User>>) {
+  userService.getAllUsers(ar -> {
     if (ar.succeeded()) {
       context.response()
         .putHeader("content-type", "application/json")
@@ -37,9 +42,9 @@ private void getAllUsers(RoutingContext context) {
   });
 }
 
-  private void addUsers(RoutingContext context) {
-    Book book = context.getBodyAsJson().mapTo(Book.class);
-    bookService.addBook(book, ar -> {
+  private void addUser(User user) {
+    User user = userService.getBodyAsJson().mapTo(User.class);
+    userService.addUser(user, ar -> {
       if (ar.succeeded()) {
         context.response()
           .putHeader("content-type", "application/json")
